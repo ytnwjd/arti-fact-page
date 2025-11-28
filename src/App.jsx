b97d2039eae5c324e54aab628dffe7472f479ba3
y.tnwjd <ysjsw@ajou.ac.kr>
2025-11-28 13:48:18 +0900

    add auth btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Login from './pages/Login'
+import Signup from './pages/Signup.jsx'
+import Profile from './pages/Profile'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      
+      <Routes>
+        <Route path="/"/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import './Header.css';
 
 export default function Header() {
+    const { user, logout } = useAuth();
+
+    const handleLogout = () => {
+        logout();
+    };
+
     return (
         <div className='header'>
             <header className='headerObj'>
@@ -10,8 +17,14 @@ export default function Header() {
                 </Link>
                 <nav>
                     <div className='nav-buttons'>
-                        <Link to="/login" className='nav-button'>Login</Link>
-                        <Link to="/profile" className='nav-button'>Profile</Link>
+                        {user ? (
+                            <>
+                                <Link to="/profile" className='nav-button'>Profile</Link>
+                                <button className='nav-button' onClick={handleLogout}>Logout</button>
+                            </>
+                        ) : (
+                            <Link to="/login" className='nav-button'>Login</Link>
+                        )}
                     </div>
                 </nav>
             </header>
